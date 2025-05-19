c0387723791cd66ea3076c1be72f0d159c4117d6
Daniela <daniela.paladinof@gmail.com>
2025-05-19 20:20:45 -0300

    feat: update to egui 0.31.1 and mark keys as pub

--- src/lib.rs
+++ src/lib.rs
@@ -52,14 +52,24 @@
 //!     )
 //! }
 //! ```
-use std::{error::Error, fmt::Display, num::ParseFloatError, ops::RangeInclusive, str::FromStr};
+use std::{
+    error::Error, fmt::Display, num::ParseFloatError,
+    ops::RangeInclusive, str::FromStr,
+};
 
-use egui::{vec2, Rect, Response, RichText, Sense, TextStyle, Ui, Vec2, WidgetText};
+use egui::{
+    vec2, Rect, Response, RichText, Sense, TextStyle, Ui, Vec2,
+    WidgetText,
+};
 
-const DO_NOT_USE_CHARS: &[char] = &['(', ')', '{', '}', '[', ']', '"', ':', ';', '|'];
+const DO_NOT_USE_CHARS: &[char] =
+    &['(', ')', '{', '}', '[', ']', '"', ':', ';', '|'];
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Deserialize, serde::Serialize)
+)]
 /// If this is not self explanatory, please read this
 /// [Wikipedia article](https://en.wikipedia.org/wiki/Modifier_key)
 /// to gain general understanding.
@@ -77,7 +87,10 @@ pub struct ParseModifierError {
 }
 
 impl Display for ParseModifierError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(
+        &self,
+        f: &mut std::fmt::Formatter<'_>,
+    ) -> std::fmt::Result {
         write!(f, "{} is not a Modifier", self.parsing)
     }
 }
@@ -99,7 +112,10 @@ impl FromStr for Modifier {
 }
 
 impl Display for Modifier {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(
+        &self,
+        f: &mut std::fmt::Formatter<'_>,
+    ) -> std::fmt::Result {
         write!(
             f,
             "{}",
@@ -113,7 +129,10 @@ impl Display for Modifier {
 }
 
 #[derive(Debug, Clone, Default, PartialEq, Eq)]
-#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Deserialize, serde::Serialize)
+)]
 /// # Keyboard key action
 /// Determines what button will do
 pub enum KeyAction {
@@ -169,16 +188,22 @@ pub enum ParseKeyActionErrorKind {
 }
 
 impl Display for ParseKeyActionError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(
+        &self,
+        f: &mut std::fmt::Formatter<'_>,
+    ) -> std::fmt::Result {
         write!(
             f,
             "{} while parsing \"{}\"",
             match self.kind {
-                ParseKeyActionErrorKind::FromText => "Expected FromText",
+                ParseKeyActionErrorKind::FromText =>
+                    "Expected FromText",
                 ParseKeyActionErrorKind::Layout => "Expected Layout",
                 ParseKeyActionErrorKind::Key => "Expected Key",
-                ParseKeyActionErrorKind::Modifier => "Expected Modifier",
-                ParseKeyActionErrorKind::Unknown => "Unexpected Token",
+                ParseKeyActionErrorKind::Modifier =>
+                    "Expected Modifier",
+                ParseKeyActionErrorKind::Unknown =>
+                    "Unexpected Token",
             },
             self.parsing,
         )
@@ -254,7 +279,10 @@ impl FromStr for KeyAction {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Deserialize, serde::Serialize)
+)]
 /// # Single keyboard button
 /// Contains text & KeyAction
 ///
@@ -298,7 +326,10 @@ pub struct ParseButtonError {
 }
 
 impl Display for ParseButtonError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(
+        &self,
+        f: &mut std::fmt::Formatter<'_>,
+    ) -> std::fmt::Result {
         write!(f, "Could not parse button from \"{}\"", self.parsing)
     }
 }
@@ -358,7 +389,9 @@ impl FromStr for Button {
         let mut search_from = 0;
         let mut split_at = inner.len();
         while let Some(colpos) = inner[search_from..].find(":") {
-            if colpos > 0 && inner.get(colpos - 1..colpos) == Some("\\") {
+            if colpos > 0
+                && inner.get(colpos - 1..colpos) == Some("\\")
+            {
                 search_from = colpos + 1;
             } else {
                 split_at = colpos + search_from;
@@ -371,7 +404,9 @@ impl FromStr for Button {
         let mut search_from = 0;
         let mut split_at = additional.len();
         while let Some(colpos) = additional[search_from..].find(";") {
-            if colpos > 0 && inner.get(colpos - 1..colpos) == Some("\\") {
+            if colpos > 0
+                && inner.get(colpos - 1..colpos) == Some("\\")
+            {
                 search_from = colpos + 1;
             } else {
                 split_at = colpos + search_from;
@@ -405,7 +440,10 @@ impl FromStr for Button {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Deserialize, serde::Serialize)
+)]
 /// # Holds a row of buttons
 /// Row can be created from string
 ///
@@ -439,7 +477,10 @@ pub struct ParseRowError {
 }
 
 impl Display for ParseRowError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(
+        &self,
+        f: &mut std::fmt::Formatter<'_>,
+    ) -> std::fmt::Result {
         write!(f, "Could not parse row from \"{}\"", self.parsing)
     }
 }
@@ -457,7 +498,11 @@ impl FromStr for Row {
             .and_then(|inner| {
                 let buttons = inner
                     .split_inclusive("}")
-                    .filter_map(|btn| Button::from_str(btn).map_err(|e| error.inner.push(e)).ok())
+                    .filter_map(|btn| {
+                        Button::from_str(btn)
+                            .map_err(|e| error.inner.push(e))
+                            .ok()
+                    })
                     .collect();
                 if error.inner.is_empty() {
                     Some(Row { buttons })
@@ -470,7 +515,10 @@ impl FromStr for Row {
 }
 
 #[derive(Debug, Clone)]
-#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Deserialize, serde::Serialize)
+)]
 /// # Named row sequence
 ///
 /// For example:
@@ -524,7 +572,10 @@ pub struct ParseLayoutError {
 }
 
 impl Display for ParseLayoutError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(
+        &self,
+        f: &mut std::fmt::Formatter<'_>,
+    ) -> std::fmt::Result {
         writeln!(f, "Could not parse layout from")?;
         writeln!(f, "\"{}\"", self.parsing)?;
         writeln!(f, "{}", self.cause)
@@ -541,19 +592,23 @@ impl FromStr for Layout {
         };
 
         let Some((l, r)) = s.split_once("\n") else {
-            error.cause = "Must have at least 2 lines: name and row".to_string();
+            error.cause = "Must have at least 2 lines: name and row"
+                .to_string();
             return Err(error);
         };
 
         if l.contains(DO_NOT_USE_CHARS) {
-            error.cause = "Layout name cannot contain (){}[]\":;".to_string();
+            error.cause =
+                "Layout name cannot contain (){}[]\":;".to_string();
             return Err(error);
         };
 
         let rows = r
             .split("\n")
             .map(Row::from_str)
-            .filter_map(|res| res.map_err(|e| error.inner.push(e)).ok())
+            .filter_map(|res| {
+                res.map_err(|e| error.inner.push(e)).ok()
+            })
             .collect();
 
         if !error.inner.is_empty() {
@@ -575,7 +630,10 @@ fn parse_key_action() {
     let action = KeyAction::from_str("Modifier(Shift)");
     assert_eq!(Ok(KeyAction::Modifier(Modifier::Shift)), action);
     let action = KeyAction::from_str("Layout(QWERTY_Mobile)");
-    assert_eq!(Ok(KeyAction::Layout("QWERTY_Mobile".to_string())), action);
+    assert_eq!(
+        Ok(KeyAction::Layout("QWERTY_Mobile".to_string())),
+        action
+    );
     let action = KeyAction::from_str("Layout(QWERTY_Mobile))");
     assert!(action.is_err());
     let action = KeyAction::from_str("FromText");
@@ -728,8 +786,14 @@ impl ModState {
     fn mod_str(&self, modifier: Modifier) -> String {
         match self {
             Self::Off => "".to_string(),
-            Self::Once => format!("_MOD_{}_ONCE", modifier.to_string().to_uppercase()),
-            Self::Hold => format!("_MOD_{}_HOLD", modifier.to_string().to_uppercase()),
+            Self::Once => format!(
+                "_MOD_{}_ONCE",
+                modifier.to_string().to_uppercase()
+            ),
+            Self::Hold => format!(
+                "_MOD_{}_HOLD",
+                modifier.to_string().to_uppercase()
+            ),
         }
     }
 }
@@ -795,7 +859,10 @@ fn mod_name() {
 /// }
 /// ```
 #[derive(Debug, Clone)]
-#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Deserialize, serde::Serialize)
+)]
 pub struct VirtualKeyboard {
     #[serde(skip)]
     mod_alt: ModState,
@@ -804,7 +871,7 @@ pub struct VirtualKeyboard {
     #[serde(skip)]
     mod_cmd: ModState,
     #[serde(skip)]
-    events: Vec<egui::Event>,
+    pub events: Vec<egui::Event>,
     /// Need to keep layouts in case user added some
     id: egui::Id,
     layouts: Vec<Layout>,
@@ -950,14 +1017,19 @@ impl VirtualKeyboard {
 
     #[inline]
     /// Use provided button width range instead of `icon_width..=interact_size.x`
-    pub fn with_button_width(mut self, width: impl Into<RangeInclusive<f32>>) -> Self {
+    pub fn with_button_width(
+        mut self,
+        width: impl Into<RangeInclusive<f32>>,
+    ) -> Self {
         self.button_w = Some(width.into());
         self
     }
 
     /// Is keyboard currently active
     pub fn is_active(&self, ctx: &egui::Context) -> bool {
-        let state = ctx.memory(|m| (m.data.get_temp::<State>(self.id).unwrap_or_default()));
+        let state = ctx.memory(|m| {
+            (m.data.get_temp::<State>(self.id).unwrap_or_default())
+        });
         state.active
     }
 
@@ -974,19 +1046,27 @@ impl VirtualKeyboard {
 
     /// Surrender keyboard focus & set keyboard as inactive
     pub fn deactivate(&self, ctx: &egui::Context) {
-        let mut state = ctx.memory(|m| (m.data.get_temp::<State>(self.id).unwrap_or_default()));
+        let mut state = ctx.memory(|m| {
+            (m.data.get_temp::<State>(self.id).unwrap_or_default())
+        });
         state.deactivate = true;
         ctx.memory_mut(|m| m.data.insert_temp(self.id, state));
     }
 
     /// What ID is keyboard focused on
     pub fn focused(&self, ctx: &egui::Context) -> Option<egui::Id> {
-        let state = ctx.memory(|m| (m.data.get_temp::<State>(self.id).unwrap_or_default()));
+        let state = ctx.memory(|m| {
+            (m.data.get_temp::<State>(self.id).unwrap_or_default())
+        });
         state.focus
     }
 
     /// Adds keyboard events to raw input
-    pub fn bump_events(&mut self, _ctx: &egui::Context, raw_input: &mut egui::RawInput) {
+    pub fn bump_events(
+        &mut self,
+        _ctx: &egui::Context,
+        raw_input: &mut egui::RawInput,
+    ) {
         raw_input.events.append(&mut self.events);
     }
     /// Add Keyboard to ui.
@@ -1025,7 +1105,8 @@ impl VirtualKeyboard {
     }
 
     fn buttons(&mut self, ui: &mut Ui) -> Response {
-        let Some(layout) = self.layouts.get(self.current).cloned() else {
+        let Some(layout) = self.layouts.get(self.current).cloned()
+        else {
             return ui.response();
         };
 
@@ -1044,9 +1125,12 @@ impl VirtualKeyboard {
             .clone()
             .unwrap_or(spacing.icon_width..=spacing.interact_size.x);
 
-        let btn_w = ((h_space - spacing.item_spacing.x * (btn_mul - 1.0)) / btn_mul)
+        let btn_w = ((h_space
+            - spacing.item_spacing.x * (btn_mul - 1.0))
+            / btn_mul)
             .clamp(*btn_w_r.start(), *btn_w_r.end());
-        let btn_h = self.button_h.unwrap_or(spacing.interact_size.x * 0.9);
+        let btn_h =
+            self.button_h.unwrap_or(spacing.interact_size.x * 0.9);
 
         let btn_std = vec2(btn_w, btn_h);
 
@@ -1058,43 +1142,64 @@ impl VirtualKeyboard {
         );
 
         let (base_id, kbd_rect) = ui.allocate_space(desired_size);
-        let base_resp = ui.interact(kbd_rect, base_id, Sense::hover());
+        let base_resp =
+            ui.interact(kbd_rect, base_id, Sense::hover());
 
         if !ui.is_rect_visible(kbd_rect) {
             return base_resp;
         }
 
         let vis = ui.style().noninteractive();
-        ui.painter()
-            .rect(kbd_rect, vis.rounding, vis.weak_bg_fill, vis.bg_stroke);
+        ui.painter().rect(
+            kbd_rect,
+            vis.corner_radius,
+            vis.weak_bg_fill,
+            vis.bg_stroke,
+            egui::StrokeKind::Middle,
+        );
 
-        let draw_btn = |button: &Button, offset: &mut Vec2, ui: &mut Ui| {
-            let kbd_min = kbd_rect.min.to_vec2();
-            Self::draw_one(kbd_min, btn_std, itm_spc, button, offset, ui)
-        };
+        let draw_btn =
+            |button: &Button, offset: &mut Vec2, ui: &mut Ui| {
+                let kbd_min = kbd_rect.min.to_vec2();
+                Self::draw_one(
+                    kbd_min, btn_std, itm_spc, button, offset, ui,
+                )
+            };
 
-        let actions = layout.rows.iter().enumerate().flat_map(|(row_idx, row)| {
-            let btn_mul = row.takes_width();
-            let mut offset = vec2(
-                (kbd_rect.width() - (btn_std + itm_spc).x * btn_mul + itm_spc.x) * 0.5,
-                (btn_std + itm_spc).y * row_idx as f32,
-            );
+        let actions = layout.rows.iter().enumerate().flat_map(
+            |(row_idx, row)| {
+                let btn_mul = row.takes_width();
+                let mut offset = vec2(
+                    (kbd_rect.width()
+                        - (btn_std + itm_spc).x * btn_mul
+                        + itm_spc.x)
+                        * 0.5,
+                    (btn_std + itm_spc).y * row_idx as f32,
+                );
 
-            let res: Vec<_> = row
-                .buttons
-                .iter()
-                .map(|button| {
-                    let resp = draw_btn(button, &mut offset, ui);
-                    (button.action.clone(), button.text.clone(), resp)
-                })
-                .collect();
-            res
-        });
+                let res: Vec<_> = row
+                    .buttons
+                    .iter()
+                    .map(|button| {
+                        let resp = draw_btn(button, &mut offset, ui);
+                        (
+                            button.action.clone(),
+                            button.text.clone(),
+                            resp,
+                        )
+                    })
+                    .collect();
+                res
+            },
+        );
 
-        let resp = actions.fold(base_resp, |acc, (action, text, response)| {
-            self.process_action(action, text, &response);
-            acc | response
-        });
+        let resp = actions.fold(
+            base_resp,
+            |acc, (action, text, response)| {
+                self.process_action(action, text, &response);
+                acc | response
+            },
+        );
 
         ui.advance_cursor_after_rect(kbd_rect);
         resp
@@ -1110,26 +1215,26 @@ impl VirtualKeyboard {
     ) -> Response {
         let min = (kbd_min + *offset).to_pos2();
         let size = vec2(
-            btn_std.x + (btn_std.x + itm_spc.x) * (button.width_mult - 1.0),
+            btn_std.x
+                + (btn_std.x + itm_spc.x) * (button.width_mult - 1.0),
             btn_std.y,
         );
         offset.x += size.x + itm_spc.x;
 
         let rect = Rect::from_min_size(min, size);
         let resp = ui.allocate_rect(rect, Sense::click());
-        let text = WidgetText::RichText(RichText::new(&button.text).monospace()).into_galley(
-            ui,
-            None,
-            size.y,
-            TextStyle::Button,
-        );
+        let text = WidgetText::RichText(
+            RichText::new(&button.text).monospace(),
+        )
+        .into_galley(ui, None, size.y, TextStyle::Button);
         let visuals = ui.style().interact(&resp);
 
         ui.painter().rect(
             rect,
-            visuals.rounding,
+            visuals.corner_radius,
             visuals.weak_bg_fill,
             visuals.bg_stroke,
+            egui::StrokeKind::Middle,
         );
         ui.painter().galley(
             (rect.center().to_vec2() - text.size() * 0.5).to_pos2(),
@@ -1141,11 +1246,13 @@ impl VirtualKeyboard {
     }
 
     pub fn layout_name(&self) -> String {
-        let Some(layout) = self.layouts.get(self.current).cloned() else {
+        let Some(layout) = self.layouts.get(self.current).cloned()
+        else {
             return "".to_string();
         };
 
-        let mid = layout.name.find("_MOD").unwrap_or(layout.name.len());
+        let mid =
+            layout.name.find("_MOD").unwrap_or(layout.name.len());
         let (no_mod_name, _r) = layout.name.split_at(mid);
         no_mod_name.to_string()
             + &self.mod_alt.mod_str(Modifier::Alt)
@@ -1153,15 +1260,23 @@ impl VirtualKeyboard {
             + &self.mod_cmd.mod_str(Modifier::Command)
     }
 
-    fn process_action(&mut self, action: KeyAction, text: String, response: &Response) {
+    fn process_action(
+        &mut self,
+        action: KeyAction,
+        text: String,
+        response: &Response,
+    ) {
         let modifiers = egui::Modifiers {
             alt: self.mod_alt != ModState::Off,
             shift: self.mod_shf != ModState::Off,
             command: self.mod_cmd != ModState::Off,
             ..Default::default()
         };
-        match (response.clicked(), response.double_clicked(), action) {
-            (true, _, KeyAction::Layout(name)) => self.switch_layout(&name),
+        match (response.clicked(), response.double_clicked(), action)
+        {
+            (true, _, KeyAction::Layout(name)) => {
+                self.switch_layout(&name)
+            }
             (true, false, KeyAction::Modifier(modifier)) => {
                 match modifier {
                     Modifier::Shift => {
@@ -1224,7 +1339,7 @@ impl VirtualKeyboard {
                         modifiers,
                     });
                 }
-                self.events.push(egui::Event::Text(text));
+                self.events.push(egui::Event::Text(text.clone()));
 
                 let mut changed = false;
                 if self.mod_alt == ModState::Once {
@@ -1258,7 +1373,8 @@ impl VirtualKeyboard {
                 });
 
                 if !text.is_empty() {
-                    self.events.push(egui::Event::Text(text.to_string()));
+                    self.events
+                        .push(egui::Event::Text(text.to_string()));
                 }
             }
 
